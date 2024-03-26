d0d4c16286937e84bee79ab4d92a96c67831d0e7
TangkaGoswamiPuja <web4dev2learning0@gmail.com>
2024-03-26 16:19:43 +0600

    first commit

---
+++ src/assets/components/Root.jsx
@@ -0,0 +1,12 @@
+import { Outlet } from "react-router-dom";
+
+const Root = () => {
+    return (
+        <div>
+            hifnvjrnj
+            <Outlet></Outlet>
+        </div>
+    );
+};
+
+export default Root;
